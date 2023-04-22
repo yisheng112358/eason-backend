11c50a8fb9bd86f16cab841f7c7e69c0a50e6fb8
Eason Chen <chenyisheng112358@gmail.com>
2023-04-22 23:17:22 +0800

    initial commit

---
+++ src/main/java/com/sideproject/easonbackend/EasonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.easonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasonBackendApplication.class, args);
+	}
+
+}
