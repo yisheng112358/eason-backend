906fd471106550ede27e978fc1d4460a6188c45a
Eason Chen <chenyisheng112358@gmail.com>
2023-06-01 23:05:55 +0800

    add spring boot test

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,11 +2,14 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -16,7 +19,7 @@ public class HospitalSetController {
     private HospitalSetService hospitalSetService;
 
     //1 查询医院设置表所有信息
-//    @ApiOperation(value = "获取所有医院设置")
+    @ApiOperation(value = "获取所有医院设置")
     @GetMapping("/findAll")
     public List<HospitalSet> findAllHospitalSet() {
         //调用service的方法
@@ -25,8 +28,8 @@ public class HospitalSetController {
         return list;
     }
 
-//    //1 查询医院设置表所有信息
-//    @ApiOperation(value = "获取所有医院设置")
+    //    //1 查询医院设置表所有信息
+    @ApiOperation(value = "获取所有医院设置")
 //    @GetMapping("findAll")
 //    public Result findAllHospitalSet() {
 //        //调用service的方法
